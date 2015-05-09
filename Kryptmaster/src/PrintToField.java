d2c56adff9b547c70bd1d8cf773f5f44a136a465
jojbak <jonathanjb1891@gmail.com>
2015-05-09 14:54:31 +0200

    kryptmaster projekt

---
+++ Kryptmaster/src/PrintToField.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * Prints output to the GUI text field
+ * @author jonathan
+ *
+ */
+public interface PrintToField {
+
+	/**
+	 * Print to GUI text field
+	 */
+	void printField(String out);
+}
