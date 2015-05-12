0a8fa03d4afb24c9bdfb680dec664eed352ccf5b
kod11 <rickard.kodet@gmail.com>
2015-05-12 19:12:10 +0200

    Now works with any number. Also uses a string for key since it fits better with rest of program.

--- Kryptmaster/src/AlgCaesar.java
+++ Kryptmaster/src/AlgCaesar.java
@@ -1,53 +1,112 @@
 import java.util.LinkedList;
 
+/**
+ * Implements the simple caesar algorithm using words as key
+ * 
+ * @author Rickard & Jonathan
+ * @version 2015-05-12
+ *
+ */
+
 public class AlgCaesar /* implements Algorithm */{
 
-	//implementera å ä och ö
-	public LinkedList encrypt(LinkedList<String> in, int key) {
+	/**
+	 * Encrypts a LinkedList containing strings using the caesar chipher.
+	 * 
+	 * @param in
+	 *            LinkedList with strings
+	 * @param key
+	 *            A password which we base the encryption on
+	 * @return LinkedList containing encrypted strings
+	 */
+	// implementera å ä och ö
+	public LinkedList<String> encrypt(LinkedList<String> in, String key) {
+		int keyInt = stringToInt(key);
 		LinkedList<String> out = new LinkedList<String>();
 		for (String str : in) {
-			// manipulera varje char med angiven nyckel
+			// manipulate each char based on the key
 			char[] chars = str.toCharArray();
-			for (int i=0; i < chars.length;i++) {
-				int val = (int) chars[i]; // skapa if-sats fï¿½r grï¿½nsen av ascii vï¿½rden
-				val = val + key;
-				
-				if (val > 126) {
-					val = 33 + (val - 126);
+			for (int i = 0; i < chars.length; i++) {
+				// turns into ascii when casted to int
+
+				int val = (int) chars[i];
+				// if proper char based on ascii value range
+
+				if (val > 32 && val < 126) {
+					// reduce for easier calculations, will increase to proper
+					// range later
+					val = val - 32;
+					// modulo to reduce into correct range
+					val = (val + keyInt) % 96;
+					if (val < 0) {
+						val += 96;
+					}
+
 				}
-				chars[i] = (char) val;
-				
-				
+				//the 32 we subtracted earlier
+				chars[i] = (char) (val + 32);
+
 			}
-			String wordOut = new String (chars);
+			String wordOut = new String(chars);
 			out.add(wordOut);
-			
+
 		}
-		
+
 		return out;
 	}
 
-	public LinkedList decrypt(LinkedList<String> in, int key) {
+	/**
+	 * Decrypts someting encrypted with the caesar algorithm, provided the key.
+	 * 
+	 * @param in
+	 *            LinkedList with encrypted strings
+	 * @param key
+	 *            String with the password
+	 * @return LinkedList with now decrypted and fully readable strings
+	 */
+
+	public LinkedList<String> decrypt(LinkedList<String> in, String key) {
 
 		LinkedList<String> out = new LinkedList<String>();
+		int keyInt = stringToInt(key);
 		for (String str : in) {
-			// manipulera varje char med angiven nyckel
+			// manipulate each char based on the key
 			char[] chars = str.toCharArray();
-			for (int i=0; i < chars.length;i++) {
-				int val = (int) chars[i]; // skapa if-sats fï¿½r grï¿½nsen av ascii vï¿½rden
-				val = val - key;
-				
-				if (val < 33) {
-					val = 126 - (val - 33);
+			for (int i = 0; i < chars.length; i++) {
+				// turns into ascii when casted to int
+				int val = (int) chars[i];
+				// if proper char based on ascii value range
+				if (val > 32 && val < 126) {
+					// reduce for easier calculations, will increase to proper
+					// range later
+					val = val - 32;
+					// modulo to reduce into correct range
+					val = (val - keyInt) % 96;
+					if (val < 0) {
+						val += 96;
+					}
 				}
-				chars[i] = (char) val;
-				
-				
+				//the 32 we subtracted earlier
+				chars[i] = (char) (val + 32);
+
 			}
-			String wordOut = new String (chars);
+			String wordOut = new String(chars);
 			out.add(wordOut);
-			
+
 		}
 		return out;
+	}
+
+	/*
+	 * Transforms a string into a number using its ascii values
+	 */
+	private int stringToInt(String key) {
+		int number = 0;
+		char[] chars = key.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			// transforms chars into ascii with cast
+			number += (int) chars[i];
+		}
+		return (number);
 	}
 }
