3ae3a9610ed9b5f70c911b4f4fcab64158d458d2
jojbak <jonathanjb1891@gmail.com>
2015-05-12 20:07:45 +0200

    changed method parameters

--- Kryptmaster/src/InputFile.java
+++ Kryptmaster/src/InputFile.java
@@ -1,22 +1,23 @@
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 import java.io.*;
-public class InputFile implements TextFileInput {
+public class InputFile /*implements TextFileInput*/ {
 	
 	private final static String NAME = InputFile.class.getName();
 	
-	public void openFile(String path)throws IOException{
-		try (BufferedReader file = new BufferedReader(new FileReader(path))) {
-	        readFile(file);
+	
+	public void openFile(File file)throws IOException{
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+	        readFile(reader);
 	    } catch (IOException e) {
 	        System.err.printf("%s: %s%n", NAME, e);
 	    }
 	}
 	
-	public LinkedList readFile(BufferedReader file)throws IOException{ 
+	public LinkedList readFile(BufferedReader reader)throws IOException{ 
 		String text;
 		LinkedList<String> in = new LinkedList<String>();
-		while((text = file.readLine()) != null){
+		while((text = reader.readLine()) != null){
 			StringTokenizer stk = new StringTokenizer(text);
 			String part = stk.nextToken();
 			in.add(part);
