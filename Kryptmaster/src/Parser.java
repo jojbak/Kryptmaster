d39fad6705fc41a9f3405ec1c075a798b156d5e2
kod11 <rickard.kodet@gmail.com>
2015-05-12 15:04:34 +0200

    Did some restructuring and fixed to work with both encryption and decryption.

--- Kryptmaster/src/Parser.java
+++ Kryptmaster/src/Parser.java
@@ -1,18 +1,25 @@
 import java.util.StringTokenizer;
 import java.util.LinkedList;
 
+/**
+ * Turns strings into LinkedLists
+ * @author Jonathan
+ *
+ */
+
 public class Parser {
 	AlgCaesar cae = new AlgCaesar();
+	LinkedList<String> output = new LinkedList<String>();
 
+	/**
+	 * Sends a string to specified encryption algorithm
+	 * @param in String to encrypt
+	 * @param key Key to use in encryption
+	 * @return String with encrypted text
+	 */
 	// anpassa för att skicka algoritmobjekt
 	public String parseEncrypt(String in, String key) {
-		StringTokenizer stk = new StringTokenizer(in);
-		LinkedList<String> input = new LinkedList<String>();
-		LinkedList<String> output = new LinkedList<String>();
-		while (stk.hasMoreTokens()) {
-			input.add(stk.nextToken());
-		}
-		output = cae.encrypt(input, Integer.parseInt(key));
+		output = cae.encrypt(stringToList(in), Integer.parseInt(key));
 		StringBuilder sb = new StringBuilder();
 		for(String word : output){
 			sb.append(word);
@@ -22,7 +29,34 @@ public class Parser {
 		return sb.toString();
 	}
 
-	public void parseDecrypt() {
+	/**
+	 * Sends a encrypted string to a specified algorithm for decryption
+	 * @param in String to decrypt
+	 * @param key Key to use
+	 * @return String with decrypted text
+	 */
+	public String parseDecrypt(String in, String key) {
+		output = cae.decrypt(stringToList(in), Integer.parseInt(key));
+		StringBuilder sb = new StringBuilder();
+		for(String word : output){
+			sb.append(word);
+			sb.append(" ");
+		}
 
+		return sb.toString();
+	}
+	/**
+	 * Transforms a string into a linked list where each object is
+	 * a word that was separated with a blank space
+	 * @param in Strint to turn into linked list
+	 * @return LinkedList with strings
+	 */
+	private LinkedList<String> stringToList(String in){
+		StringTokenizer stk = new StringTokenizer(in);
+		LinkedList<String> input = new LinkedList<String>();
+		while (stk.hasMoreTokens()) {
+			input.add(stk.nextToken());
+		}
+		return input;
 	}
 }
