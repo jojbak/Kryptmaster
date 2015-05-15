c79d6fa434cf7a1be5bfd2d52aae28c0bd37e764
kod11 <rickard.kodet@gmail.com>
2015-05-15 12:57:40 +0200

    added TODO

---
+++ Kryptmaster/src/AlgEasteregg.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * Fun easter egg where the text is "encrypted" into 1337-5p34k.
+ * @author rickard & jonathan
+ * @version 2015-05-15
+ *
+ */
+public class AlgEasteregg implements Algorithm{
+
+	HashMap<Character,String> map = new HashMap<Character,String>();
+	
+	@Override
+	public LinkedList<String> encrypt(LinkedList<String> in, String key) {
+		return null;
+	}
+
+	@Override
+	public LinkedList<String> decrypt(LinkedList<String> in, String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	/**
+	 * Generate the proper values for each letter
+	 */
+	private void generateAlphabet(){
+	}
+
+}
